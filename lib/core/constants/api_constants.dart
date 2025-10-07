6fc12bfb8c57847bd7b14dfca18b72f304283557
Rasha Alaswad <rasha.n.alaswad@gmail.com>
2025-10-07 15:42:16 +0300

    Add API error handling and constants
    - Add ApiConstants for API response keys.
    - Add ApiUrls for API endpoint management.
    - Implemented ApiErrorModel for structured error responses.
    - Add ApiErrorHandler to manage and interpret Dio exceptions.
    - Added ApiResult class for handling success and failure states.
    - Add DioFactory for configuring Dio instance with interceptors and headers.

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String statusCode = 'statusCode';
+  static const String message = 'message';
+  static const String errors = 'errors';
+}
---
+++ lib/core/networking/api_urls.dart
@@ -0,0 +1,5 @@
+class ApiUrls {
+  ApiUrls._();
+  static const String baseUrl = "https://accessories-eshop.runasp.net/api/";
+  static const String register = "auth/register";
+}
